aa9f405b4bc209938d6db23ea1cf9467a95e9634
SavageCore <talk@savagecore.uk>
2025-03-13 01:54:03 +0000

    Fix HUNO cloudflare detection

--- trackers/huno.py
+++ trackers/huno.py
@@ -20,7 +20,7 @@ class HunoTracker(BaseTracker):
         # Open the profile URL
         self.driver.get(profile_url)
 
-        if self.driver.find_elements(By.ID, "challenge-running"):
+        if self.driver.find_elements(By.XPATH, "//*[contains(text(), 'Verify you are human by completing the action below.')]"):
             print(
                 "Please solve the challenge and press enter to continue or type 'n' and press enter to skip."
             )
