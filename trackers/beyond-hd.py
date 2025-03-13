bb7f5eed7a13f891470ab80cb7ad69a59624d021
SavageCore <talk@savagecore.uk>
2025-03-13 01:49:00 +0000

    Fix BHD

--- trackers/beyond-hd.py
+++ trackers/beyond-hd.py
@@ -1,4 +1,5 @@
 import pyotp
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 from utils import get_decrypted_secrets
@@ -31,20 +32,20 @@ class BeyondhdTracker(BaseTracker):
         username_field.send_keys(username)
         password_field.send_keys(password)
 
-        # Click on the login button
-        login_button.click()
-
-        # 2FA is enabled, ask for the code
-        code_field = self.driver.find_element(By.ID, "code")
-        if code_field:
-            if self.config["settings"]["auto_2fa"] == "true":
-                decrypted_secrets = get_decrypted_secrets()
-                totp = pyotp.TOTP(decrypted_secrets[self.tracker_name])
-                code = totp.now()
-            else:
-                code = input("Please enter the 2FA code: ")
-            code_field.send_keys(code)
-            login_button = self.driver.find_element(By.ID, "login-button")
+        try:
+            # 2FA is enabled, ask for the code
+            code_field = self.driver.find_element(By.ID, "code")
+            if code_field:
+                if self.config["settings"]["auto_2fa"] == "true":
+                    decrypted_secrets = get_decrypted_secrets()
+                    totp = pyotp.TOTP(decrypted_secrets[self.tracker_name])
+                    code = totp.now()
+                else:
+                    code = input("Please enter the 2FA code: ")
+                code_field.send_keys(code)
+                login_button = self.driver.find_element(By.ID, "login-button")
+                login_button.click()
+        except NoSuchElementException:
             login_button.click()
 
         self.driver.get(profile_url)
