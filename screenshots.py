3b66d1e379a537b3d7d3621d54c3f294e1c2f39b
SavageCore <talk@savagecore.uk>
2025-03-13 00:51:41 +0000

    Aither 2FA support

--- screenshots.py
+++ screenshots.py
@@ -79,6 +79,13 @@ if "ATH" in config["wanted-trackers"]["trackers"]:
 
     # Login and save screenshot
     driver.find_element(By.CLASS_NAME, "auth-form__primary-button").click()
+
+    # 2FA is enabled, ask for the code
+    if driver.find_elements(By.ID, "code"):
+        code = input("Please enter the 2FA code: ")
+        code_field = driver.find_element(By.ID, "code")
+        code_field.send_keys(code)
+
     take_screenshot("Aither")
     print("Aither Screenshoted")
 
